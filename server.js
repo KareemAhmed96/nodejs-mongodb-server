380d05aa92f588f4c8a786db8d838d03189d4cee
kareemzak <kareemahmed96@outlook.com>
2021-03-28 18:26:04 +0200

    added some comments

--- server.js
+++ server.js
@@ -15,39 +15,42 @@ app.get("/",function(req,res){
     res.send({message:"welcome"});
 });
 
-
+//Get record (document)
 app.get("/api/:col",function(req,res){
     app.db.collection(req.params.col).find(req.query).toArray(function(err,data){
         res.send(data);
     });
 })
 
+//Get record (document) with filter query params http://localhost:8080/api/students?name=<searchName>
 app.post("/api/:col/filter",bodyParserJson,function(req,res){
     app.db.collection(req.params.col).find(req.body).toArray(function(err,data){
         res.send(data);
     });
 })
 
+//Send record (document)
 app.post("/api/:col",bodyParserJson,function(req,res){
-
     app.db.collection(req.params.col).insertOne(req.body);
     res.send({message:req.params.col+" added successfully"})
 })
 
-
+//Update record (document)
 app.put("/api/:col",bodyParserJson,function(req,res){
     app.db.collection(req.params.col).updateOne({_id:req.body._id},{$set:req.body});
     res.send({message:req.params.col+" updated successfully"})
 })
 
+//Delete record (document)
 app.delete("/api/:col/:id",function(req,res){
-    let id = new ObjectId(req.params.id);
+    let id = Number(req.params.id);
     app.db.collection(req.params.col).removeOne({_id:id});
     res.send({message:req.params.col+" deleted"});
 })
 
+//Get one record (document)
 app.get("/api/:col/:id",function(req,res){
-    app.db.collection(req.params.col).findOne({_id:new ObjectId(req.params.id)},function(err,data){
+    app.db.collection(req.params.col).findOne({_id:Number(req.params.id)},function(err,data){
         res.send(data);
     });
     
